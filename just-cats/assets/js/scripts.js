c088af852c03c69fdf0e6f17de8f4d0eb2fbb8c4
ThatsJojo <cleyton.eu123@gmail.com>
2022-07-24 09:24:11 -0300

    Tratando exceções para o consumo da API thecatapi

--- just-cats/assets/js/scripts.js
+++ just-cats/assets/js/scripts.js
@@ -2,8 +2,12 @@ const img = document.getElementById("cat");
 const URL = "https://api.thecatapi.com/v1/images/search?format=json&limit=1"
 
 const atualizarImagem = async() => {
-    let data = await fetch(URL).then(res => res.json());
-    img.src = data[0].url;
+    try {
+        let data = await fetch(URL).then(res => res.json());
+        img.src = data[0].url;
+    } catch (e) {
+        console.log(e);
+    }
 }
 atualizarImagem();
 document.getElementById('change-cat').onclick = atualizarImagem;
